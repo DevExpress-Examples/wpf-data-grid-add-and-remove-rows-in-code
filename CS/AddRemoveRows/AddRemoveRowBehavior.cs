3975e2cb9e26f7b8a3ebb9b4cd689ce6fe6274b9
volkov.anton <anton.volkov@devexpress.com>
2021-11-01 13:10:51 +0300

    Update

---
+++ CS/AddRemoveRows/AddRemoveRowBehavior.cs
@@ -0,0 +1,48 @@
+﻿using DevExpress.Mvvm;
+using DevExpress.Mvvm.UI.Interactivity;
+using DevExpress.Xpf.Grid;
+using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+
+namespace AddRemoveRows {
+    public class AddRemoveRowBehavior : Behavior<TableView> {
+        public static DependencyPropertyKey AddCommandPropertyKey = DependencyProperty.RegisterReadOnly(nameof(AddCommand), typeof(ICommand), typeof(AddRemoveRowBehavior), new PropertyMetadata(null));
+        public static DependencyProperty AddCommandProperty;
+        public ICommand AddCommand {
+            get { return (ICommand)GetValue(AddCommandProperty); }
+            private set { SetValue(AddCommandPropertyKey, value); }
+        }
+
+        public static DependencyPropertyKey DeleteCommandPropertyKey = DependencyProperty.RegisterReadOnly(nameof(DeleteCommand), typeof(ICommand), typeof(AddRemoveRowBehavior), new PropertyMetadata(null));
+        public static DependencyProperty DeleteCommandProperty;
+        public ICommand DeleteCommand {
+            get { return (ICommand)GetValue(DeleteCommandProperty); }
+            private set { SetValue(DeleteCommandPropertyKey, value); }
+        }
+
+        public AddRemoveRowBehavior() {
+            AddCommand = new DelegateCommand(AddNewRow);
+            DeleteCommand = new DelegateCommand(DeleteRow);
+        }
+
+        protected override void OnAttached() {
+            base.OnAttached();
+            AssociatedObject.InitNewRow += OnInitNewRow;
+        }
+
+        void AddNewRow() {
+            AssociatedObject.AddNewRow();
+        }
+        void OnInitNewRow(object sender, InitNewRowEventArgs e) {
+            AssociatedObject.Grid.SetCellValue(GridControl.NewItemRowHandle, AssociatedObject.Grid.Columns["ProductName"], "New Product");
+            AssociatedObject.Grid.SetCellValue(GridControl.NewItemRowHandle, AssociatedObject.Grid.Columns["UnitPrice"], 10);
+            AssociatedObject.Grid.SetCellValue(GridControl.NewItemRowHandle, AssociatedObject.Grid.Columns["CompanyName"], "New Company");
+            AssociatedObject.Grid.SetCellValue(GridControl.NewItemRowHandle, AssociatedObject.Grid.Columns["Discontinued"], false);
+        }
+        void DeleteRow() {
+            AssociatedObject.DeleteRow(AssociatedObject.FocusedRowHandle);
+        }
+    }
+}
---
+++ CS/AddRemoveRows/MainViewModel.cs
@@ -0,0 +1,33 @@
+﻿using DevExpress.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace AddRemoveRows {
+    public class Person {
+        public string ProductName { get; set; }
+        public string CompanyName { get; set; }
+        public int UnitPrice { get; set; }
+        public bool Discontinued { get; set; }
+
+        public Person() { }
+        public Person(int i) {
+            ProductName = "ProductName" + i;
+            CompanyName = "CompanyName" + i;
+            UnitPrice = i;
+            Discontinued = i % 2 == 0;
+        }
+    }
+    public class MainViewModel : ViewModelBase {
+        public static ObservableCollection<Person> Persons { get; private set; }
+        public MainViewModel() {
+            Persons = new ObservableCollection<Person>(GetSource());
+        }
+        static IEnumerable<Person> GetSource() {
+            for(int i = 0; i < 9; i++) {
+                yield return new Person(i);
+            }
+        }
+    }
+}
